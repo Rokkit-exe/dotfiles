262c38c0dbbecab919ee2473a74a02f349a237ca
Rokkit-exe <francki0723@gmail.com>
2025-06-09 17:30:34 -0400

    adding ansible playbook to chezmoi config

---
+++ run_once_before_init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script installs Ansible on a Linux system.
+
+# checking if ansible is already installed
+if [ -x "$(command -v ansible)" ]; then
+  echo "Ansible is already installed."
+  exit 0
+else
+  echo "Ansible is not installed. Proceeding with installation..."
+  sudo pacman -Syu --noconfirm ansible
+fi
+echo "Ansible has been successfully installed."
