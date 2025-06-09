fee8d28e8f30e754ea523de760d8ad69bbecee82
Rokkit-exe <francki0723@gmail.com>
2025-06-09 17:32:22 -0400

    adding system update even if ansible is installed

--- run_once_before_init.sh
+++ run_once_before_init.sh
@@ -5,6 +5,8 @@
 # checking if ansible is already installed
 if [ -x "$(command -v ansible)" ]; then
   echo "Ansible is already installed."
+  echo "Updating system packages..."
+  sudo pacman -Syu --noconfirm
   exit 0
 else
   echo "Ansible is not installed. Proceeding with installation..."
