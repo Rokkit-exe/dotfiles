f9e07d166f1bc5fd2c0320fcc068dd373593c28d
Rokkit-exe <francki0723@gmail.com>
2025-06-19 20:29:54 -0400

    fixing typo

--- dot_playbook/scripts/virt_config.sh
+++ dot_playbook/scripts/virt_config.sh
@@ -8,9 +8,9 @@ sudo systemctl enable --now libvirtd
 sudo systemctl start libvirtd
 
 # Configure libvirt
-sudo sed -i "s|^.*unix_sock_group.*$|unix_sock_group="libvirt"|" /etc/libvirt/libvirtd.conf
-sudo sed -i "s|^.*unix_sock_rw_perms.*$|unix_sock_rw_perms="0770"|" /etc/libvirt/libvirtd.conf
-sudo sed -i "s|^.*unix_sock_ro_perms.*$|unix_sock_ro_perms="0777"|" /etc/libvirt/libvirtd.conf
+sudo sed -i 's|^.*unix_sock_group.*$|unix_sock_group="libvirt"|' /etc/libvirt/libvirtd.conf
+sudo sed -i 's|^.*unix_sock_rw_perms.*$|unix_sock_rw_perms="0770"|' /etc/libvirt/libvirtd.conf
+sudo sed -i 's|^.*unix_sock_ro_perms.*$|unix_sock_ro_perms="0777"|' /etc/libvirt/libvirtd.conf
 
 # Add current user to libvirt group
 sudo usermod -aG libvirt $(whoami)
