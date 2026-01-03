2d3e0518fb8cbdd7ae7efacfc8f3b2d355a80ce0
Francis Di-Folco <francki0723@gmail.com>
2026-01-03 14:06:22 -0500

    feat: add ouput messages

--- re-add.sh
+++ re-add.sh
@@ -4,6 +4,7 @@
 #
 chezmoi cd
 
+echo "re-adding all tracked files"
 chezmoi re-add "$(chezmoi list)"
 
 git add .
@@ -12,6 +13,10 @@ git add .
 date=$(date +"%Y-%m-%d %T")
 git commit -m "re-add all tracked configs on $date"
 
-git push origin main
+git push origin main || {
+  echo "unable to push to origin"
+  exit 1
+}
 
+echo "sucessfully pushed to origin"
 
